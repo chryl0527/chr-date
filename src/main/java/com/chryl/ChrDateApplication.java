9ffd009f518df056b0be4363e4630933cc5027b1
chr0527 <1015670600@qq.com>
2019-05-24 14:48:08 +0800

    Initial commit

---
+++ src/main/java/com/chryl/ChrDateApplication.java
@@ -0,0 +1,13 @@
+package com.chryl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrDateApplication.class, args);
+	}
+
+}
